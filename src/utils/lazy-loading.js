758b096321e47ee2d6e1696a931ed3c1912219fa
Athype <kozari.krisztian@gmail.com>
2024-01-28 18:38:44 +0100

    updated site with new data
    new pics added, prices updated

--- src/utils/lazy-loading.js
+++ src/utils/lazy-loading.js
@@ -16,6 +16,22 @@ const lazyLoading = () => {
     lazyImgs.forEach(img => {
         observer.observe(img);
     })
+
+    const lazy2images = document.querySelectorAll('.lazy2');
+    lazy2images.forEach(img => {
+        console.log(img, 'set loading')
+        img.classList.add('loading');
+        const onLoad = () => {
+            console.log(img, 'loaded')
+            img.classList.remove('loading');
+            img.classList.add('loaded');
+        }
+        if (img.complete) {
+            onLoad();
+        } else {
+            img.addEventListener('load', onLoad, {once: true});
+        }
+    })
 }
 
 export default lazyLoading;
